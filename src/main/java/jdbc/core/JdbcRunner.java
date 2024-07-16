a1dcea502c77fba4fde0d754c6b6620bbe44527b
v.muravskyi <v.muravskyi@gmail.com>
2024-07-16 21:18:29 +0200

    save

--- src/main/java/jdbc/core/JdbcRunner.java
+++ src/main/java/jdbc/core/JdbcRunner.java
@@ -4,13 +4,25 @@ import jdbc.core.util.ConnectionManager;
 
 import java.sql.Connection;
 import java.sql.SQLException;
+import java.sql.Statement;
 
 public class JdbcRunner {
 
     public static void main(String[] args) throws SQLException {
 
-        Connection connection = ConnectionManager.open();
-        System.out.println(connection.getTransactionIsolation());
+        String sql = """
+                CREATE TABLE IF NOT EXISTS info (
+                    id SERIAL PRIMARY KEY,
+                    data TEXT NOT NULL
+                );
+                """;
+        try (Connection connection = ConnectionManager.open();
+             Statement statement = connection.createStatement()) {
+            System.out.println(connection.getSchema());
+            System.out.println(connection.getTransactionIsolation());
+            boolean execute = statement.execute(sql);
+            System.out.println(execute);
+        }
 
     }
 
