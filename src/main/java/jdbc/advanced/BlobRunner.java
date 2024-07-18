f1de33ab99f6eb74ac5b00415e6f6a4427994535
v.muravskyi <v.muravskyi@gmail.com>
2024-07-19 00:22:57 +0200

    add blob and clob

---
+++ src/main/java/jdbc/advanced/BlobRunner.java
@@ -0,0 +1,100 @@
+package jdbc.advanced;
+
+import jdbc.util.ConnectionManager;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.StandardOpenOption;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class BlobRunner {
+
+    public static void main(String[] args) {
+        // blob (binary lange object), bytea
+        // clob (character large object), TEXT
+
+//        saveImageToDb();
+        getImage();
+    }
+
+    private static void getImage() {
+        var sql = """
+                SELECT image
+                FROM aircraft
+                WHERE id = ?
+                """;
+
+        try (Connection connection = ConnectionManager.open()) {
+            connection.setAutoCommit(false);
+            PreparedStatement preparedStatement = connection.prepareStatement(sql);
+            preparedStatement.setLong(1, 1);
+
+            ResultSet resultSet = preparedStatement.executeQuery();
+
+            if (resultSet.next()) {
+                InputStream image = resultSet.getBinaryStream("image");
+                byte[] allBytes = image.readAllBytes();
+                Path outputPath = Path.of("src/main/resources", "newFile.jpeg");
+                Files.write(outputPath, allBytes, StandardOpenOption.CREATE);
+            }
+
+            connection.commit();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private static void saveImageToDb() {
+        var sql = """
+                UPDATE aircraft
+                SET image = ?
+                WHERE id = ?
+                """;
+        try (Connection connection = ConnectionManager.open()) {
+            connection.setAutoCommit(false);
+
+            PreparedStatement preparedStatement = connection.prepareStatement(sql);
+            preparedStatement.setBytes(1, Files.readAllBytes(Path.of("src/main/resources/boeing-777.jpeg")));
+            preparedStatement.setLong(2, 1);
+
+            int result = preparedStatement.executeUpdate();
+            connection.commit();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+//    private static void saveImageToDb() {
+//        var sql = """
+//                UPDATE aircraft
+//                SET image = ?
+//                WHERE id = ?
+//                """;
+//        try (Connection connection = ConnectionManager.open()) {
+//            connection.setAutoCommit(false);
+//            Blob blob = connection.createBlob();
+//            blob.setBytes(1, Files.readAllBytes(Path.of("resources", "boeing-777.jpeg")));
+//
+//            PreparedStatement preparedStatement = connection.prepareStatement(sql);
+//            preparedStatement.setBlob(1, blob);
+//            preparedStatement.setLong(2, 1);
+//
+//            int result = preparedStatement.executeUpdate();
+//            connection.commit();
+//        } catch (SQLException e) {
+//            throw new RuntimeException(e);
+//        } catch (IOException e) {
+//            throw new RuntimeException(e);
+//        }
+//    }
+
+}
