b38fbf92214ec40289afe236782aada84b76cb4e
v.muravskyi <v.muravskyi@gmail.com>
2024-07-21 11:52:44 +0200

    removed unused methods

--- src/main/java/jdbc/DaoRunner.java
+++ src/main/java/jdbc/DaoRunner.java
@@ -39,30 +39,4 @@ public class DaoRunner {
         System.out.println("Deleted ticket:\n" + deleted + "\n");
     }
 
-    private static void findById() {
-        TicketDao ticketDao = TicketDao.getInstance();
-        Optional<TicketEntity> ticket = ticketDao.getById(72L);
-        System.out.println(ticket + "\n");
-    }
-
-    private static void delete() {
-        TicketDao ticketDao = TicketDao.getInstance();
-        boolean deleted = ticketDao.delete(100L);
-        System.out.println(deleted + "\n");
-    }
-
-    private static void save() {
-        TicketDao ticketDao = TicketDao.getInstance();
-
-        TicketEntity ticketEntity = new TicketEntity()
-                .setPassengerNo("JHY789")
-                .setPassengerName("Freddy Mercury")
-                .setFlightId(3L)
-                .setSeatNo("F4")
-                .setCost(new BigDecimal("123.99"));
-
-        TicketEntity savedTicket = ticketDao.save(ticketEntity);
-        System.out.println(savedTicket + "\n");
-    }
-
 }
